4a10b595b0214bbe1884c13189aa0bca5a70b54b
Introvert Hedgehog <larby2409@gmail.com>
2023-04-05 02:15:08 +0800

    live demo

--- pages/api/embbeding.js
+++ pages/api/embbeding.js
@@ -1,6 +1,6 @@
 export default async function (req, res) {
   try {
-    const top3 = await fetch("http://127.0.0.1:5000/embbeding", {
+    const top3 = await fetch("https://oyster-app-99rix.ondigitalocean.app/embbeding", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
