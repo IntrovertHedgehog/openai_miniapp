c6335bbaa55df92b4e81dc27bd2a9ae198ce4478
IntrovertHedgehog <larby2409@gmail.com>
2023-05-26 22:06:09 +0800

    change server

--- pages/api/root.js
+++ pages/api/root.js
@@ -1,2 +1,3 @@
-export default "https://oyster-app-99rix.ondigitalocean.app"
+// export default "https://oyster-app-99rix.ondigitalocean.app"
 // export default "http://127.0.0.1:5000"
+export default "http://206.189.44.47"
